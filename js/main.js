69102a926da310b9f85df1c4d9b95a947c96f372
MaaBosch <47601532+MaaBosch@users.noreply.github.com>
2019-05-08 18:58:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const testData = [[1, 20, 30], [20, 1, 60], [30, 60, 1]]; // Constant data for testing purposes
+const testData2 = [[0, 6, 8, 9, 8, 8], [5, 6, 2, 7, 4, 0], [9, 7, 6, 2, 9, 8], [2, 6, 0, 3, 6, 1], [5, 8, 7, 6, 9, 7], [4, 1, 5, 3, 3, 1]]; // Constant data for testing purposes V2
 
 var matrix = new Matrix()
-//matrix.setData(testData)
-matrix.randomize()
-//matrix = matrix.optimalLeafOrder()
+matrix.setData(testData2)
+//matrix.randomize()
+matrix = matrix.barycenterOrder()
 
 var vis = new MatrixVisualization(matrix, 'canvas');
