fe5ca17a025ade02302a4d22c70bf8361dc0cadc
Dirksen <k.d.m.dirksen@student.tue.nl>
2019-05-13 14:31:38 +0200

    testing

--- php/clear_uploads_folder.php
+++ php/clear_uploads_folder.php
@@ -4,8 +4,9 @@
 	
 	$files = glob('../uploads/*'); // get all file names
 	foreach($files as $file){ // iterate files
-	  if(is_file($file))
-		unlink($file); // delete file
+	  if(is_file($file)) {
+			unlink($file); // delete file
+		}
 	}
 	
 	$locatie = "../matrixNodelink.php";
