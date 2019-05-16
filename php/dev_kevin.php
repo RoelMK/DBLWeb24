874df251ce87d5cfa74ff0d2aeced7f661962604
Dirksen <k.d.m.dirksen@student.tue.nl>
2019-05-16 14:42:34 +0200

    developer codes

---
+++ php/dev_kevin.php
@@ -0,0 +1,6 @@
+<?php 
+	include ("setup.inc.php"); 
+?>
+<a href="dev_clear_uploads_code.php">dev_clear_uploads_code</a><br>
+<a href="dev_all_uploads_code.php">dev_all_uploads_code</a><br>
+<a href="dev_all_share_code.php">dev_all_share_code</a><br>
